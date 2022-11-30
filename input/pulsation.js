33cf83867223addec8c4964819fad2402fe5c63a
Deniss Strombergs <deniss.strombergs@scandiweb.com>
2022-11-30 22:07:47 +0200

    Update pulsation

--- input/pulsation.js
+++ input/pulsation.js
@@ -1,34 +1,30 @@
-document.body.style.margin = 0
-const vss = 'uniform float s;void main(){gl_Position=vec4(0,0,0,1);gl_PointSize=s;}'
-const fss = 'precision mediump float;uniform float v;void main(){float r=ceil(cos(radians(gl_FragCoord.x))+cos(radians(gl_FragCoord.y))+v),g=cos(r),b=cos(g);gl_FragColor=vec4(r,g,b,1);}'
-
-const c = document.createElement('canvas')
-const w = window.innerWidth
-const h = window.innerHeight
-c.width = w
-c.height = h
+c = document.createElement('canvas')
+w = c.width = window.innerWidth
+h = c.height = window.innerHeight
 document.body.append(c)
-const gl = c.getContext('webgl')
 
-const vs = gl.createShader(gl.VERTEX_SHADER)
-gl.shaderSource(vs, vss)
-gl.compileShader(vs)
+a = `void main(){gl_Position=vec4(0,0,0,1);gl_PointSize=${w > h ? w : h}.;}`
+b = `precision mediump float;uniform float t;void main(){float r=ceil(cos(radians(gl_FragCoord.x))+cos(radians(gl_FragCoord.y))+t),g=cos(r),b=cos(g);gl_FragColor=vec4(r,g,b,1);}`
+g = c.getContext('webgl')
 
-const fs = gl.createShader(gl.FRAGMENT_SHADER)
-gl.shaderSource(fs, fss)
-gl.compileShader(fs)
+v = g.createShader(g.VERTEX_SHADER)
+g.shaderSource(v, a)
+g.compileShader(v)
 
-const p = gl.createProgram()
-gl.attachShader(p, vs)
-gl.attachShader(p, fs)
-gl.linkProgram(p)
-gl.useProgram(p)
+f = g.createShader(g.FRAGMENT_SHADER)
+g.shaderSource(f, b)
+g.compileShader(f)
 
-const vl = gl.getUniformLocation(p, 'v')
-gl.uniform1f(gl.getUniformLocation(p, 's'), w > h ? w : h)
+p = g.createProgram()
+g.attachShader(p, v)
+g.attachShader(p, f)
+g.linkProgram(p)
+g.useProgram(p)
 
-{(d = () => {
-    gl.uniform1f(vl, performance.now() / 1000)
-    gl.drawArrays(gl.POINTS, 0, 1)
+t = g.getUniformLocation(p, 't')
+
+{(d = _ => {
+    g.uniform1f(t, performance.now() / 2000)
+    g.drawArrays(g.POINTS, 0, 1)
     requestAnimationFrame(d)
 })()}
